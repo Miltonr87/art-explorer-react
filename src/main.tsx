b28838593537295f3b3fabe7d061ec95ea4a3eb3
Miltonr87 <miltonrodrigues713@gmail.com>
2025-08-01 18:38:42 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './scss/main.scss';
-import { ThemeProvider } from './ThemeContext';
+import { ThemeProvider } from './context/ThemeContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
